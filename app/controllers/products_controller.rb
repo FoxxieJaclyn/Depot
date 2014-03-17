857ebacbaf8dbc0ce68d29159f33b4d0625844b5
Jaclyn Rosa <Emmitt.lemon.fox@gmail.com>
2014-03-17 18:53:34 -0500

    depot 13err

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -62,7 +62,7 @@ class ProductsController < ApplicationController
   end
 
 	def who_bought
-		@product = Product.find)params[:id])
+		@product = Product.find(params[:id])
 		@latest_order = @product.orders.order(:updated_at).last
 		if stale?(@latest_order)
 			respond_to do |format|
